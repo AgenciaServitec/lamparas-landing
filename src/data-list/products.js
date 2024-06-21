625d16261095d6491d301d9335cf0f7da5516093
noel <admin@agenciaservitec.com>
2024-06-21 03:30:42 -0500

    initial project

---
+++ src/data-list/products.js
@@ -0,0 +1,78 @@
+export const products = [
+  {
+    imagePath: "./images/5jj3s05001-benq.webp",
+    name: "5J.J3S05.001 para BENQ",
+  },
+  {
+    imagePath: "./images/5jj6v05001.webp",
+    name: "5J.J6V05.001 para BENQ MS502",
+  },
+  {
+    imagePath: "./images/bl-fp240e.webp",
+    name: "BL-FP240E para OPTOMA",
+  },
+  {
+    imagePath: "./images/bl-fp280c.webp",
+    name: "BL-FP280c para OPTOMA",
+  },
+  {
+    imagePath: "./images/dt01021.webp",
+    name: "DT01021 para HITACHI",
+  },
+  {
+    imagePath: "./images/dt01025.webp",
+    name: "DT01025 para HITACHI",
+  },
+  {
+    imagePath: "./images/et-lal100.webp",
+    name: "ET-LAL100 para PANASONIC\n",
+  },
+  {
+    imagePath: "./images/et-lal510.webp",
+    name: "ET-LAL510 para PANASONIC\n",
+  },
+  {
+    imagePath: "./images/lmp-d213.webp",
+    name: "LMP-D213 para SONY",
+  },
+  {
+    imagePath: "./images/lmp-e212.webp",
+    name: "LMP-E212 para SONY",
+  },
+  {
+    imagePath: "./images/elplp50.webp",
+    name: "ELPLP50 para EPSON",
+  },
+  {
+    imagePath: "./images/elplp75.webp",
+    name: "ELPLP75 para EPSON",
+  },
+  {
+    imagePath: "./images/elplp76.webp",
+    name: "ELPLP76 para EPSON",
+  },
+  {
+    imagePath: "./images/elplp79.webp",
+    name: "ELPLP79 para EPSON",
+  },
+  {
+    imagePath: "./images/elplp88.webp",
+    name: "ELPLP88 para EPSON",
+  },
+  {
+    imagePath: "./images/elplp91.webp",
+    name: "ELPLP91 para EPSON",
+  },
+  {
+    imagePath: "./images/elplp95.webp",
+    name: "ELPLP95 para EPSON",
+  },
+  {
+    imagePath: "./images/elplp96.webp",
+    name: "ELPLP96 para EPSON",
+  },
+  {
+    imagePath: "./images/elplp97.webp",
+    name: "ELPLP79 para EPSON",
+  },
+];
