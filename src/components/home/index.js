3145ce7abd2cef9b1cdddae121f2b3dc0c005774
Ignacio2906 <ignacio.malcer@gmail.com>
2024-07-09 12:06:05 -0500

    delete brand section and fix code

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,5 +1,3 @@
 export { default as CarouselProducts } from "./CarouselProducts.astro";
 export { default as AboutUsComponent } from "./AboutUs.astro";
-export { default as BrandsSection } from "./BrandsSection.astro";
 export { default as SeoSection } from "./SeoSection.astro";
-export { default as ContactSection } from "./ContactSection.astro";
